3ee778174ace772aeb81ab8d5d16f8f61cc7e6ee
MaciejWolak <maciejwolak@gmail.com>
2021-06-25 17:08:37 +0200

    generate app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CartManagerComponent } from './components/admin/cart-manager/cart-manager.component';
+import { OrderManagerComponent } from './components/admin/order-manager/order-manager.component';
+import { ProductManagerComponent } from './components/admin/product-manager/product-manager.component';
+import { UserManagerComponent } from './components/admin/user-manager/user-manager.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { FooterComponent } from './components/public/shered/footer/footer.component';
+import { NavComponent } from './components/public/shered/nav/nav.component';
+import { HeaderComponent } from './components/public/shered/header/header.component';
+import { PageNotFoundComponent } from './components/public/shered/page-not-found/page-not-found.component';
+import { ProductListComponent } from './components/public/shopping-cart/product-list/product-list.component';
+import { ProductComponent } from './components/public/shopping-cart/product-list/product/product.component';
+import { CartListComponent } from './components/public/shopping-cart/cart-list/cart-list.component';
+import { CartComponent } from './components/public/shopping-cart/cart-list/cart/cart.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartManagerComponent,
+    OrderManagerComponent,
+    ProductManagerComponent,
+    UserManagerComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    NavComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
+    ProductListComponent,
+    ProductComponent,
+    CartListComponent,
+    CartComponent
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/admin/cart-manager/cart-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-manager',
+  templateUrl: './cart-manager.component.html',
+  styleUrls: ['./cart-manager.component.css']
+})
+export class CartManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
